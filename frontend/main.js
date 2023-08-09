5e9db62242022158a205b48c99599b3b12638dfe
Joyson Fernandes <joysonfernandes619@gmail.com>
2023-08-09 18:02:37 +0100

    Added frontend folder

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const FunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count =  response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
